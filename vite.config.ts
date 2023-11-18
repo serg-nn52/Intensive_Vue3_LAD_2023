ca602d372298d83f80c10db3edd41766b45c47f7
Sergey Lushkin <serg.nn1985@gmail.com>
2023-11-19 02:01:04 +0300

    добавил gh-pages, скрипты для деплоя

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,15 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
+  base: '/Intensive_Vue3_LAD_2023',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-})
+});
